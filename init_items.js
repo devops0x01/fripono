fee2c4b05be83d1279ebb73eeb5a334b8385f221
devops0x01 <devops0x01@gmail.com>
2017-08-19 17:50:53 -0700

    Switched protection 2 amulet to the correct graphic.

--- init_items.js
+++ init_items.js
@@ -24,7 +24,7 @@ function generate_jewelry(level)
     break;
     case 1:
       l = [
-        function(){return make_protection_jewelry("amulet of protection 2",2,3,"neck",2,"a greater amulet of protection");},
+        function(){return make_protection_jewelry("amulet of protection 2",1,3,"neck",2,"a greater amulet of protection");},
         function(){return make_protection_jewelry("amulet of protection 1",0,3,"neck",1,"a greater amulet of protection");},
         function(){return make_protection_jewelry("greater ring of protection",2,2,[],2,"a greater ring of protection");}
       ];
