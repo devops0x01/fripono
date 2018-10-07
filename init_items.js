83d63aaf402b556c13daba4f7ef17b10a5e70bad
devops0x01 <devops0x01@gmail.com>
2018-10-07 14:11:02 -0700

    Add item boots of regrowth
    Events need to be re-added for the life regen feature that should go with this item.

--- init_items.js
+++ init_items.js
@@ -679,6 +679,18 @@ var unique_items = [
     
     return i;
   },
+  
+    function boots_of_regrowth(){
+    let i = new Item("Boots of Regrowth",new Tile(armor,1,9),0,0);
+    i.tile.description = "Boots of Regrowth";
+    i.slot_type = "feet";
+    i.armor = 6;
+    i.history = "dum ditty dum ditty dum dum dum...";
+    i.onEquip = function(){this.equipped = true;p.armor_augment += i.armor;p.updateStats();};
+    i.onUnequip = function(){this.equipped = false;p.armor_augment -= i.armor;p.updateStats();};
+    
+    return i;
+  },
 
   function gloves_of_defense(){
     let i = new Item("Gloves of Defense",new Tile(armor,7,2),0,0);
